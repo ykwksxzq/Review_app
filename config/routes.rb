bdb86b7d3ade2d0f05cf581d4d4466457e6ed821
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2024-01-19 17:00:09 +0000

    homesコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
- 
+
 # 顧客用
 # URL /customers/sign_in ...
-devise_for :customers,skip: [:passwords], controllers: {
+devise_for :users,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
