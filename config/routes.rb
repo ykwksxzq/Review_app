bd4fbc486ffb9574d036fe14f20fd16e556bcb7e
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2024-01-19 14:15:41 +0000

    deviseの導入

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+ 
+# 顧客用
+# URL /customers/sign_in ...
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+end
