cfdecc1b5123ca630d33c9986848c2331e948cb2
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2024-01-23 07:47:13 +0000

    ActiveStorageの導入

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+
+has_one_attached :image
+
 end
--- db/migrate/20240122084046_create_post_tags.rb
+++ db/migrate/20240122084046_create_post_tags.rb
@@ -2,10 +2,13 @@ class CreatePostTags < ActiveRecord::Migration[6.1]
   def change
     create_table :post_tags do |t|
 
-      t.integer :tag_id, null: false
-      t.integer :post_id,  null: false
+      t.references :post, foreign_key: true
+      t.references :tag, foreign_key: true
 
       t.timestamps
     end
+    
+      # 同じタグを２回保存するのは出来ないようになる
+      add_index :post_tags, [:post_id, :tag_id], unique: true
   end
 end
