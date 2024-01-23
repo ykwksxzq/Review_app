01d359d130d6a79465cc9179ccd536b04d4dd382
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2024-01-23 08:08:29 +0000

    リレーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,16 @@
 class Post < ApplicationRecord
 
-has_one_attached :image
+  has_one_attached :image
+
+  belongs_to :user
+  has_many :reviews, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  belongs_to :genre
+  belongs_to :shop
+
+  has_many :post_tags,dependent: :destroy
+  has_many :tags,through: :post_tags
 
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,5 @@
 class Shop < ApplicationRecord
+
+  has_many :post, dependent: :destroy
+
 end
